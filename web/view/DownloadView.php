4d13664f796c0eb003ad5902f9a3cdaeac2000ee
Marta Johnsson <marta@longislanddivers.com>
2013-01-19 17:45:55 +0100

    download view

--- web/view/DownloadView.php
+++ web/view/DownloadView.php
@@ -5,27 +5,38 @@ class DownloadView {
     public function doDownloadLinks() {
             $html = 
 			'
-				<h1>Downloads</h1>
-
-				<div class="search">
-					<p>
-						<a href="https://github.com/downloads/jensevertsson/ProjectX/Snippt.exe">
-							<img src="content/image/snippt_downloadlink_windows.png" alt="Download Snippt for Windows" title="Download Snippt for Windows" />
-						</a>
-					</p>
-	
-					<p>
-						<a href="https://github.com/downloads/jensevertsson/ProjectX/Snippt.dmg">
-							<img src="content/image/snippt_downloadlink_macosx.png" alt="Download Snippt for Mac OS X" title="Download Snippt for Mac OS X" />
-						</a>
-					</p>
-
-					<p>
-						<a href="https://github.com/downloads/jensevertsson/ProjectX/snippt-1.0-3-i686.els">
-							<img src="content/image/snippt_downloadlink_enlisy.png" alt="Download Snippt for Enlisy" title="Download Snippt for Enlisy" />
-						</a>
-					</p>
-				</div>
+            <div class="row">
+                <div class="span12">
+                    <div class="inner">
+                        <h1>Downloads</h1>
+                    </div>  
+                    <div class="row">
+                        <div class="span4">
+                            <div class="inner">
+                                <a href="https://github.com/downloads/jensevertsson/ProjectX/Snippt.exe">
+                                    <img src="content/image/icons/win/icon_256.png" alt="Download Snippt for Windows" title="Download Snippt for Windows" />
+                                </a>
+                            </div>
+                        </div>
+                        
+                        <div class="span4"> 
+                            <div class="inner"> 	
+        						<a href="https://github.com/downloads/jensevertsson/ProjectX/Snippt.dmg">
+        							<img src="content/image/icons/mac/icon_256.png" alt="Download Snippt for Mac OS X" title="Download Snippt for Mac OS X" />
+        						</a>
+                            </div>
+                        </div>
+                        
+                        <div class="span4">
+                            <div class="inner">
+        						<a href="https://github.com/downloads/jensevertsson/ProjectX/snippt-1.0-3-i686.els">
+        							<img src="content/image/icons/enlisy/icon_256.png" alt="Download Snippt for Enlisy" title="Download Snippt for Enlisy" />
+        						</a>
+                            </div>
+                        </div>
+                    </div>                    
+                </div>
+            </div>
 			';
             return $html;
     }
